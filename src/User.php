73cd55f5ef3aaab72d767a098e661f5c43638a81
Rustam <rustamergashev.sp@gmail.com>
2024-08-08 19:41:55 +0500

    MINOR: User class refactored

--- src/User.php
+++ src/User.php
@@ -8,17 +8,22 @@ use PDO;
 
 class User
 {
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = Db::connect();
+    }
+
     public function create(
         string $username,
         string $position,
         string $gender,
         string $phone
     ): false|array {
-        $pdo = DB::connect();
-
         $query = "INSERT INTO users (username, position, gender, phone, created_at)
                   VALUES (:username, :position, :gender, :phone, NOW())";
-        $stmt  = $pdo->prepare($query);
+        $stmt  = $this->pdo->prepare($query);
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':position', $position);
         $stmt->bindParam(':gender', $gender);
@@ -30,9 +35,8 @@ class User
 
     public function getUser(int $id)
     {
-        $pdo   = DB::connect();
         $query = "SELECT * FROM users WHERE id = :id";
-        $stmt  = $pdo->prepare($query);
+        $stmt  = $this->pdo->prepare($query);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
@@ -44,11 +48,10 @@ class User
         string $position,
         string $gender,
         string $phone
-    ) {
-        $pdo   = DB::connect();
+    ): void {
         $query = "UPDATE users SET username = :username, position = :position, gender = :gender, phone = :phone, updated_at = NOW()
                   WHERE id = :id";
-        $stmt  = $pdo->prepare($query);
+        $stmt  = $this->pdo->prepare($query);
         $stmt->bindParam(':id', $id);
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':position', $position);
@@ -57,11 +60,10 @@ class User
         $stmt->execute();
     }
 
-    public function deleteUser(int $id)
+    public function deleteUser(int $id): void
     {
-        $pdo   = DB::connect();
         $query = "DELETE FROM users WHERE id = :id";
-        $stmt  = $pdo->prepare($query);
+        $stmt  = $this->pdo->prepare($query);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
     }
