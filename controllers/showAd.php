7cffa829f010719771c0e7e5f7d3c14a4e50766c
Rustam <rustamergashev.sp@gmail.com>
2024-08-19 20:18:42 +0500

    MAJOR: New UI

--- controllers/showAd.php
+++ controllers/showAd.php
@@ -1,7 +1,9 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * @var $id
+ */
 $ad = (new \App\Ads())->getAd($id);
 
 loadView('single-ad', ['ad' => $ad]);
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 use App\Router;
 
-Router::get('/', fn()=> loadView('home'));
+Router::get('/', fn()=> loadController('home'));
 
 Router::get('/ads/{id}', function (int $id) {
     loadController('showAd', ['id'=>$id]);
 });
 
-Router::get('/ads/create', fn()=> loadView('admin/create-ad'));
+Router::get('/ads/create', fn()=> loadView('dashboard/create-ad'));
 Router::post('/ads/create', fn()=> loadController('createAd'));
 
 Router::errorResponse(404, 'Not Found');
